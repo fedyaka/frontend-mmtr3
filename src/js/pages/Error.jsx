e7bed9b1d9ba28bacc34243749ae3a6b0fbb3d2d
fedyaka <68501592+fedyaka@users.noreply.github.com>
2022-12-16 17:38:35 +0300

    теперь точно всё!
    фикс фона и добавлен архив с конечными файлами.

--- src/js/pages/Error.jsx
+++ src/js/pages/Error.jsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react'
 
 export default class Error extends Component {
   render() {
-    return (<>
+    return (<div className='error'>
       <h1>Ошибочка</h1>
       <h2>Такой страницы не существует</h2>
-      <img src='/img/errorCat.png'></img>
-    </>)
+      <div className='error__img'></div>
+    </div>)
   }
 }
